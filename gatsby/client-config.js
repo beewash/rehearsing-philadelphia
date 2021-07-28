821227ba4db835e913ca9bb227ba533668bbc000
Bryan Washington <beewash92@gmail.com>
2021-07-28 11:43:27 -0400

    Initial Commit

--- gatsby/client-config.js
+++ gatsby/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '8hj1t7km',
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'ibfjhn1r',
     dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
