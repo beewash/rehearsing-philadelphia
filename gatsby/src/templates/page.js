92bbd3dd6f64988f7df5d8b5c30725e9067d06b7
Bryan Washington <beewash92@gmail.com>
2021-08-01 18:08:50 -0400

    Update Layout path

--- gatsby/src/templates/page.js
+++ gatsby/src/templates/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {graphql} from 'gatsby'
-import Layout from '../components/layout/Layout'
+import Layout from '../components/Layout/Layout'
 import PageBuilder from '../components/pageBuilder'
 
 export const query = graphql`
