fdff37b10c776a939c7c209ab67e6e4037a96931
Bryan Washington <beewash92@gmail.com>
2021-08-24 13:25:09 -0400

    Add map component
    - Update map.js

--- gatsby/src/components/Map/map.js
+++ gatsby/src/components/Map/map.js
@@ -9,14 +9,6 @@ import locations from './locations.json'
 const location = [39.9526, -75.1652]
 const zoom = 14
 
-delete L.Icon.Default.prototype._getIconUrl
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-})
-
 function MapComp() {
 
   // REFS
