a6bf97273eaa9ebb1aa17a8c29895f21c558e912
Bryan Washington <beewash92@gmail.com>
2021-09-13 09:57:29 -0400

    Fix buttons

--- gatsby/src/components/button.js
+++ gatsby/src/components/button.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Button({button, filter}) {
     return (
-        <div id="filter" className="buttons w-full flex flex-wrap flex-row space-x-4 space-y-4 mb-8">
+        <div id="filter" className="buttons w-full flex flex-wrap flex-row space-x-4 space-y-4 mb-8 items-end">
             {
                 button.map((cat, i)=>{
-                    return <button type="button" onClick={()=> filter(cat)} className="btn w-max py-1 px-4 font-acuminPro font-medium uppercase text-black text-sm border-black border-2 rounded-full hover:bg-black focus:bg-black hover:text-white focus:text-white">{cat}</button>
+                    return <button type="button" onClick={()=> filter(cat)} className="btn h-8 w-max py-1 px-4 font-acuminPro font-medium uppercase text-black text-sm border-black border-2 rounded-full hover:bg-black focus:bg-black hover:text-white focus:text-white">{cat}</button>
                 })
             }
         </div>
