dd280db213a081f8d21b7968aabe580486b0ef88
Bryan Washington <beewash92@gmail.com>
2021-09-17 16:37:44 -0400

    Update hiddenPage.js

--- gatsby/src/pages/hiddenPage.js
+++ gatsby/src/pages/hiddenPage.js
@@ -13,6 +13,12 @@ const HiddenPage = () => (
     <div className="col-span-3 md:col-span-3 lg:col-span-3"></div>
     <div className="col-span-4 md:col-span-4 lg:col-span-4"></div>
     <div className="col-span-5 md:col-span-5 lg:col-span-5"></div>
+    <div className="hover:text-black"></div>
+    <h1></h1>
+    <h2></h2>
+    <h3></h3>
+    <h4></h4>
+    <h5></h5>
   </div>
 )
 
