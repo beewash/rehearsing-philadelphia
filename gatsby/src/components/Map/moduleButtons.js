efdc737e04dba04dec55ddc3c03c09517159d1cb
Bryan Washington <beewash92@gmail.com>
2022-03-11 10:38:54 -0500

    fix map key styling

--- gatsby/src/components/Map/moduleButtons.js
+++ gatsby/src/components/Map/moduleButtons.js
@@ -7,38 +7,38 @@ import communityFlatIcon from '../../images/communityFlatIcon.png'
 
 function ModuleButton({ filter }) {
   return (
-    <ul className="flex flex-row flex-wrap justify-between space-y-2">
-      <li className="flex flex-row items-center space-x-2 w-1/2 lg:w-1/3">
-        <button id="allButton" onClick={() => filter('all')}>
+    <ul className="flex flex-row flex-wrap justify-between">
+      <li className="w-fit p-2">
+        <button id="allButton" className="flex flex-row items-center space-x-2" onClick={() => filter('all')}>
           <p className="font-acuminPro uppercase text-cfsSM1">All</p>
         </button>
       </li>
-      <li className="flex flex-row items-center space-x-2 w-1/2 lg:w-1/3">
-        <button id="soloButton" onClick={() => filter('solo')}>
+      <li className="w-fit p-2">
+        <button id="soloButton" className="flex flex-row items-center space-x-2" onClick={() => filter('solo')}>
           <img src={soloFlatIcon} className="w-4 h-auto" alt="solo" />
           <p className="font-acuminPro uppercase text-cfsSM1">Solo</p>
         </button>
       </li>
-      <li className="flex flex-row items-center space-x-2 w-1/2 lg:w-1/3">
-        <button id="duetButton" onClick={() => filter('duet')}>
+      <li className="w-fit p-2">
+        <button id="duetButton" className="flex flex-row items-center space-x-2" onClick={() => filter('duet')}>
           <img src={duetFlatIcon} className="w-4 h-auto" alt="duet" />
           <p className="font-acuminPro uppercase text-cfsSM1">Duet</p>
         </button>
       </li>
-      <li className="flex flex-row items-center space-x-2 w-1/2 lg:w-1/3">
-        <button id="ensembleButton" onClick={() => filter('ensemble')}>
+      <li className="w-fit p-2">
+        <button id="ensembleButton" className="flex flex-row items-center space-x-2" onClick={() => filter('ensemble')}>
           <img src={ensembleFlatIcon} className="w-4 h-auto" alt="ensemble" />
           <p className="font-acuminPro uppercase text-cfsSM1">Ensemble</p>
         </button>
       </li>
-      <li className="flex flex-row items-center space-x-2 w-1/2 lg:w-1/3">
-        <button id="orchestraButton" onClick={() => filter('orchestra')}>
+      <li className="w-fit p-2">
+        <button id="orchestraButton" className="flex flex-row items-center space-x-2" onClick={() => filter('orchestra')}>
           <img src={orchestraFlatIcon} className="w-4 h-auto" alt="orchestra" />
           <p className="font-acuminPro uppercase text-cfsSM1">Orchestra</p>
         </button>
       </li>
-      <li className="flex flex-row items-center space-x-2">
-        <button id="communityButton" onClick={() => filter('community')}>
+      <li className="w-fit p-2">
+        <button id="communityButton" className="flex flex-row items-center space-x-2" onClick={() => filter('community')}>
           <img src={communityFlatIcon} className="w-4 h-auto" alt="community" />
           <p className="font-acuminPro uppercase text-cfsSM1">Artist Neighborhoods</p>
         </button>
