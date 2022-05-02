8ed2863cdf9debe694d1fa96aba0b5f15c98d8c5
Bryan Washington <beewash92@gmail.com>
2022-05-02 10:26:42 -0400

    update imports in gatsby-ssr

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,11 +1,13 @@
-const React = require('react')
-const { AnimatePresence } = require('framer-motion')
-const Radio = require('./src/components/Radio/radio')
-const SimpleReactLightbox = require('simple-react-lightbox')
-const RadioContextProvider = require('./src/context/RadioContextProvider')
+import React from 'react'
+import {AnimatePresence} from 'framer-motion'
+import "./src/styles/tailwind.css"
+import '/node_modules/slick-carousel/slick/slick.css'
+import '/node_modules/slick-carousel/slick/slick-theme.css'
+import Radio from './src/components/Radio/radio'
+import SimpleReactLightbox from 'simple-react-lightbox'
+import RadioContextProvider from './src/context/RadioContextProvider'
 
-exports.wrapPageElement = ({element}) => {
-  return (
+export const wrapPageElement = ({element}) => (
   <RadioContextProvider>
     <AnimatePresence exitBeforeEnter>
       <SimpleReactLightbox>
@@ -14,5 +16,21 @@ exports.wrapPageElement = ({element}) => {
     </AnimatePresence>
     <Radio />
   </RadioContextProvider>
-  )
-}
+)
+
+// const React = require('react')
+// const { AnimatePresence } = require('framer-motion')
+// const Radio = require('./src/components/Radio/radio')
+// const SimpleReactLightbox = require('simple-react-lightbox')
+// const RadioContextProvider = require('./src/context/RadioContextProvider')
+
+// exports.wrapPageElement = ({element}) => (
+//   <RadioContextProvider>
+//     <AnimatePresence exitBeforeEnter>
+//       <SimpleReactLightbox>
+//         {element}
+//       </SimpleReactLightbox>
+//     </AnimatePresence>
+//     <Radio />
+//   </RadioContextProvider>
+// )
